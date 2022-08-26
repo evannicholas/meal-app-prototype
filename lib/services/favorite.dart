f9de4d073770350b3a2a613e441143eb10f5dfd3
Evan Nicholas Abednego <evannicholasabednego@gmail.com>
2022-08-26 19:50:35 +0700

    added doc.data prevention in favorite services

--- lib/services/favorite.dart
+++ lib/services/favorite.dart
@@ -6,13 +6,14 @@ Future<List<MealClass>> loadFavoriteContents() async {
   List<MealClass> favorites = [];
 
   for (String mealID in currentUser!.likes) {
-    var doc =
-        await FirebaseFirestore.instance.collection("foods").doc(mealID).get();
-    var tags = (doc.data()!['tags'] as List).map((x) => x as String).toList();
-    MealClass dummyMeal = MealClass(doc.id, doc.data()!['image_url'],
-        doc.data()!['name'], tags, doc.data()!['detail'], doc.data()!['ingredients']);
+    var doc = await FirebaseFirestore.instance.collection("foods").doc(mealID).get();
+    
+    if(doc.data() != null){
+      var tags = (doc.data()!['tags'] as List).map((x) => x as String).toList();
+      MealClass dummyMeal = MealClass(doc.id, doc.data()!['image_url'],doc.data()!['name'], tags, doc.data()!['detail'], doc.data()!['ingredients']);
+      favorites.add(dummyMeal);
+    }
 
-    favorites.add(dummyMeal);
   }
 
   return favorites;
